3edaf4e5b411e9d68da8005437f5db7c4275dfde
Panos K <lkullolli@gmail.com>
2020-07-21 11:41:46 +0400

    init commit

---
+++ src/components/Home.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <>
+      <section id="home">
+        <h1>Welcome To My Site</h1>
+        <p className="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, quis!</p>
+      </section>
+      <section id="about">
+        <h1>About</h1>
+        <p className="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, quis!</p>
+      </section>
+      <section id="service">
+        <h1>Service</h1>
+        <p className="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, quis!</p>
+      </section>
+      <section id="contact">
+        <h1>Contact</h1>
+        <p className="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, quis!</p>
+      </section>
+    </>
+  );
+}
+
+export default Home;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#service">Service</a></li>
+        <Link to="#contact">Contact</Link>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
